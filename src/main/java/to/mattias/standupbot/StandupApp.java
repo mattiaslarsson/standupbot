d6d4dd5bdbcbc25ef4c643d0cb374b70e46a1e7e
Mattias Larsson <mattias@mattias.to>
2022-04-13 03:09:30 +0200

    Refactoring a bit

--- src/main/java/to/mattias/standupbot/StandupApp.java
+++ src/main/java/to/mattias/standupbot/StandupApp.java
@@ -4,6 +4,7 @@ import static java.lang.String.format;
 
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.context.builtin.SlashCommandContext;
+import com.slack.api.bolt.handler.builtin.SlashCommandHandler;
 import com.slack.api.bolt.jetty.SlackAppServer;
 import com.slack.api.bolt.response.Response;
 import com.slack.api.methods.SlackApiException;
@@ -15,21 +16,31 @@ import java.util.List;
 public class StandupApp {
 
   private static List<String> userIds;
-  private static List<String> userNames;
   private static int seconds;
+  private static App app;
 
   public static void main(String[] args) throws Exception {
-    var app = new App();
+    app = new App();
+    app.command("/standup", startStandupHandler());
+    app.command("/standup-seconds", setTimeHandler());
 
-    app.command("/standup", (req, ctx) -> {
+    var server = new SlackAppServer(app);
+    server.start();
+  }
+
+  static SlashCommandHandler startStandupHandler() {
+    return (req, ctx) -> {
       var token = ctx.getBotToken();
       userIds = new ArrayList<>();
-      userNames = new ArrayList<>();
       var text = req.getPayload().getText();
       if (text == null) {
         return sendUsageError(ctx);
       }
-      trimUsers(text);
+      try {
+        trimUsers(text);
+      } catch (IllegalArgumentException e) {
+        sendUsageError(ctx);
+      }
       if (userIds.isEmpty()) {
         return sendUsageError(ctx);
       }
@@ -42,20 +53,19 @@ public class StandupApp {
       new Thread(timer).start();
 
       return ctx.ack(format("Starting standup%n%d seconds each", seconds));
-    });
+    };
+  }
 
-    app.command("/standup-seconds", (req, ctx) -> {
+  static SlashCommandHandler setTimeHandler() {
+    return (req, ctx) -> {
+      System.out.println(req.getHeaders());
       seconds = Integer.parseInt(req.getPayload().getText());
       return ctx.ack(format("Setting the time to %d seconds", seconds));
-    });
-
-    var server = new SlackAppServer(app);
-    server.start();
-
+    };
   }
 
   private static Response sendUsageError(SlashCommandContext ctx) {
-    return ctx.ack(format("You must tag the participant users%n\"/standup @user1 "
+    return ctx.ack(format("You must tag the participating users%n\"/standup @user1 "
         + "@user2\""));
   }
 
@@ -65,14 +75,17 @@ public class StandupApp {
             .replace(">", "")
             .replace("@", "")
             .split(" ");
-
-    for (var id : ids) {
-      userIds.add(id.substring(0, id.indexOf("|")));
+    try {
+      for (var id : ids) {
+        userIds.add(id.substring(0, id.indexOf("|")));
+      }
+    } catch (StringIndexOutOfBoundsException e) {
+      throw new IllegalArgumentException(e);
     }
   }
 
-  private static void sendPersonalMessage(App app, String channel, String token) throws SlackApiException,
-      IOException {
+  private static void sendPersonalMessage(App app, String channel, String token)
+      throws SlackApiException, IOException {
     StringBuilder sb = new StringBuilder();
     for (String id : userIds) {
       sb.append("<@").append(id).append(">").append("\n");
